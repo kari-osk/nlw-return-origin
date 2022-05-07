16fb4a06ab1bf51a16013096702b6f22d03a00e9
kari-osk <karinaosuka@gmail.com>
2022-05-07 16:01:57 -0300

    add mobile

--- impulse/server/src/use-cases/submit-feedback-use-case.ts
+++ impulse/server/src/use-cases/submit-feedback-use-case.ts
@@ -43,6 +43,8 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
         `<p>Tipo de feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot? `<img src="${screenshot}" />` : null,
+        `</div>`
       ].join('\n')
 
     })
